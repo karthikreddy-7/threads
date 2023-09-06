c110b14b0ab0e5d5f86e02d349ea406d61fbb673
Basupally Karthik Reddy <basupallykarthikreddy@gmail.com>
2023-09-06 23:31:53 +0530

    community

--- components/shared/Bottombar.tsx
+++ components/shared/Bottombar.tsx
@@ -1,44 +1,45 @@
 "use client";
-import { sidebarLinks } from "@/constants";
+
 import Image from "next/image";
 import Link from "next/link";
-import { usePathname, useRouter } from "next/navigation";
-import { SignOutButton, SignedIn, useAuth } from "@clerk/nextjs";
+import { usePathname } from "next/navigation";
 
-function Bottombar(){
-    const pathname = usePathname();
-    const { userId } = useAuth();
-    return (
-        <section className="bottombar">
-            <div className="bottombar_container">
-            {sidebarLinks.map((link) => {
+import { sidebarLinks } from "@/constants";
+
+function Bottombar() {
+  const pathname = usePathname();
+
+  return (
+    <section className="bottombar">
+      <div className="bottombar_container">
+        {sidebarLinks.map((link) => {
           const isActive =
             (pathname.includes(link.route) && link.route.length > 1) ||
             pathname === link.route;
-
-          if (link.route === "/profile") link.route = `${link.route}/${userId}`;
 
           return (
             <Link
               href={link.route}
               key={link.label}
-              className={`leftsidebar_link ${isActive && "bg-primary-500 "}`}
+              className={`bottombar_link ${isActive && "bg-primary-500"}`}
             >
               <Image
                 src={link.imgURL}
                 alt={link.label}
-                width={24}
-                height={24}
+                width={16}
+                height={16}
+                className="object-contain"
               />
 
-              <p className='text-subtle-medium text-light-1 max-sm:hidden'>{link.label.split(/\s+/ )[0]}</p>
+              <p className="text-subtle-medium text-light-1 max-sm:hidden">
+                {link.label.split(/\s+/)[0]}
+              </p>
             </Link>
           );
         })}
-            </div>
-
-        </section>
-    )
+      </div>
+    </section>
+  );
 }
 
 export default Bottombar;
