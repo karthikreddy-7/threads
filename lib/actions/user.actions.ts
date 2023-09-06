319b00a794b688fa225dcc1387c39870cca3c919
Basupally Karthik Reddy <basupallykarthikreddy@gmail.com>
2023-09-06 13:46:05 +0530

    UserDb

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,6 +1,32 @@
-"use server"
+"use server";
+import { revalidatePath } from "next/cache";
+import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
 
-export async function updateUser():Promise<void>{
-    connectToDB();
+export async function updateUser(
+  userId: string,
+  username: string,
+  name: string,
+  bio: string,
+  image: string,
+  path: string
+): Promise<void> {
+  connectToDB();
+  try {
+    
+  await User.findOneAndUpdate(
+    {
+      id: userId,
+    },
+    { usernmae: username.toLowerCase(), name, bio, image, onboarded: true },
+    {upsert:true}
+  );
+
+  if (path==="/profile/edit"){
+    revalidatePath(path);
+  }
+  }
+  catch(error:any){
+    throw new Error('Failed to create/update user: ${error.message} ')
+  }
 }
