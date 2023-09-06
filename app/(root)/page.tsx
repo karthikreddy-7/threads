71bb415ffdf9e71ea0c13510c1159624fbdcc2dc
Basupally Karthik Reddy <basupallykarthikreddy@gmail.com>
2023-09-06 19:45:24 +0530

    threads

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,10 @@
 import { UserButton } from "@clerk/nextjs";
- 
-export default function Home() {
+
+export default async function Home() {
+  //await result=fetchPosts();
   return (
     <>
       <h1 className="head-text text-left">Home</h1>
     </>
-  )
+  );
 }
