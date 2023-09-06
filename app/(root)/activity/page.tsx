fc642562d18169d7142c778788b0f34c07cd13bd
Basupally Karthik Reddy <basupallykarthikreddy@gmail.com>
2023-09-06 23:04:16 +0530

    components

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,61 +1,51 @@
-import { redirect } from "next/navigation";
+import Image from "next/image";
+import Link from "next/link";
 import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-import UserCard from "@/components/cards/UserCard";
-import Searchbar from "@/components/shared/Searchbar";
-import Pagination from "@/components/shared/Pagination";
+import { fetchUser, getActivity } from "@/lib/actions/user.actions";
 
-import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
-
-async function Page({
-  searchParams,
-}: {
-  searchParams: { [key: string]: string | undefined };
-}) {
+async function Page() {
   const user = await currentUser();
   if (!user) return null;
 
   const userInfo = await fetchUser(user.id);
   if (!userInfo?.onboarded) redirect("/onboarding");
 
-  const result = await fetchUsers({
-    userId: user.id,
-    searchString: searchParams.q,
-    pageNumber: searchParams?.page ? +searchParams.page : 1,
-    pageSize: 25,
-  });
+  const activity = await getActivity(userInfo._id);
 
   return (
-    <section>
-      <h1 className="head-text mb-10">Search</h1>
+    <>
+      <h1 className="head-text">Activity</h1>
 
-      <Searchbar routeType="search" />
-
-      <div className="mt-14 flex flex-col gap-9">
-        {result.users.length === 0 ? (
-          <p className="no-result">No Result</p>
-        ) : (
+      <section className="mt-10 flex flex-col gap-5">
+        {activity.length > 0 ? (
           <>
-            {result.users.map((person) => (
-              <UserCard
-                key={person.id}
-                id={person.id}
-                name={person.name}
-                username={person.username}
-                imgUrl={person.image}
-                personType="User"
-              />
+            {activity.map((activity) => (
+              <Link key={activity._id} href={`/thread/${activity.parentId}`}>
+                <article className="activity-card">
+                  <Image
+                    src={activity.author.image}
+                    alt="user_logo"
+                    width={20}
+                    height={20}
+                    className="rounded-full object-cover"
+                  />
+                  <p className="!text-small-regular text-light-1">
+                    <span className="mr-1 text-primary-500">
+                      {activity.author.name}
+                    </span>{" "}
+                    replied to your thread
+                  </p>
+                </article>
+              </Link>
             ))}
           </>
+        ) : (
+          <p className="!text-base-regular text-light-3">No activity yet</p>
         )}
-      </div>
-
-      <Pagination
-        path="search"
-        pageNumber={searchParams?.page ? +searchParams.page : 1}
-        isNext={result.isNext}
-      />
-    </section>
+      </section>
+    </>
   );
 }
 
