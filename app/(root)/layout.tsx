015659accf32d8e5c04c571a85bd30fc06b2ea33
Basupally Karthik Reddy <basupallykarthikreddy@gmail.com>
2023-09-07 13:01:47 +0530

    final commit added right side bar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -37,8 +37,8 @@ export default function RootLayout({
             <section className="main-container">
               <div className="w-full max-w-4xl">{children}</div>
             </section>
-            {/* @ts-ignore 
-            <RightSidebar />*/}
+            {/* @ts-ignore */}
+            <RightSidebar />
           </main>
 
           <Bottombar />
